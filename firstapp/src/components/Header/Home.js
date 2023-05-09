3a207d0da24788a073410967e0588edd6824edc8
Roshroy <roshroygeorge@gmail.com>
2023-05-09 15:46:23 +0530

    5th day

--- firstapp/src/components/Header/Home.js
+++ firstapp/src/components/Header/Home.js
@@ -1,36 +1,22 @@
 import React,{useState, useEffect} from "react";
 //API
-import API from "../../API";
+
+
+
 //config
 import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from "../../config";
 // Components
  
-//Hook
-
+//Hook 
+// import { useHomeFetch } from "../../Hooks/useHomeFetch";
+import { useHomefetch } from "../../Hooks/useHomeFetch";
 //image
 
+
 const Home =  () =>{
-    const [state, setState] = useState();
-    const [loading,setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    const fetchMovies = async (page, searchTerm ="") =>{
-        try{
-       setError(false);
-       setLoading(true);
-
-       const movies = await API.fetchMovies(searchTerm,page);
-       console.log(movies);
-
-        } catch(error){
-            setError(true);
-        }
-    };
-
-    //Initial render
-    useEffect(() =>{
-      fetchMovies(1)
-    },[])
+   
+   const { state, loading, error} = useHomefetch();
+    console.log(state);
 
     return <div>Home Page</div>
 };

8a9db82d568d6f10f8805c9b434d38d8f961e688
Roshroy <roshroygeorge@gmail.com>
2023-05-10 23:54:14 +0530

    6th day 3 changes

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -2,4 +2,12 @@ import React from "react";
 //styles
 import { Wrapper,Content, Text } from "./HeroImage.styles";
 
-const HeroImage = (prop)
+const HeroImage = (prop) =>(
+    <Wrapper image={prop.image}>
+        <Content>
+            <Text>
+                <h1>{prop.title}</h1>
+            </Text>
+        </Content>
+    </Wrapper>
+)
